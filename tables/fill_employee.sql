076634b5f76f56cff78c20a41883c7ade5a1d7f4
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-12 23:06:55 -0600

    Update fill_employee.sql

--- tables/fill_employee.sql
+++ tables/fill_employee.sql
@@ -1,12 +1,24 @@
 DECLARE 
-    first_name VARCHAR2(50);
-    last_name VARCHAR2(50);
-    salary NUMBER;
-    hire_date DATE;
+  first_names VARCHAR2(32767) := 'John,Mary,Steve,Emily,Robert,Karen,Paul,Emma,David,Linda';
+  last_names VARCHAR2(32767) := 'Smith,Johnson,Williams,Brown,Jones,Miller,Davis,Garcia,Rodriguez,Wilson';
+  first_name VARCHAR2(50);
+  last_name VARCHAR2(50);
+  salary NUMBER;
+  hire_date DATE;
+  random_index_first_name NUMBER;
+  random_index_last_name NUMBER;
 BEGIN
   FOR i IN 1..20 LOOP
-    first_name := DBMS_RANDOM.STRING('A', 10);
-    last_name := DBMS_RANDOM.STRING('A', 10);
+    random_index_first_name := TRUNC(DBMS_RANDOM.VALUE(1, 11));
+    random_index_last_name := TRUNC(DBMS_RANDOM.VALUE(1, 11));
+    first_name := SUBSTR(first_names, 
+                         INSTR(first_names || ',', ',', 1, random_index_first_name) + 1, 
+                         INSTR(first_names || ',', ',', 1, random_index_first_name + 1) - 
+                         INSTR(first_names || ',', ',', 1, random_index_first_name) - 1);
+    last_name := SUBSTR(last_names, 
+                        INSTR(last_names || ',', ',', 1, random_index_last_name) + 1, 
+                        INSTR(last_names || ',', ',', 1, random_index_last_name + 1) - 
+                        INSTR(last_names || ',', ',', 1, random_index_last_name) - 1);
     salary := DBMS_RANDOM.VALUE(30000, 80000);
     hire_date := SYSDATE - DBMS_RANDOM.VALUE(1, 3650);
     INSERT INTO employees (firstname, lastname, salary, hire_date)
