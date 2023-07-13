0854b7c8cd6f8c18ca502205f260ea4ae5707815
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-13 13:28:15 -0600

    Update getEmployeeName.sql

--- functions/getEmployeeName.sql
+++ functions/getEmployeeName.sql
@@ -1,9 +1,9 @@
-CREATE FUNCTION getEmployeeName (p_employee_id IN NUMBER) 
+CREATE OR REPLACE FUNCTION getEmployeeName (p_employee_id IN NUMBER) 
 RETURN VARCHAR2 
 IS 
   employee_name VARCHAR2(100);
 BEGIN
-  SELECT first_name || ' ' || last_name 
+  SELECT firstname || ' ' || lastname 
   INTO employee_name 
   FROM employees 
   WHERE employee_id = p_employee_id;
