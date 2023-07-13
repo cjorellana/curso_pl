5f7309f84c1f939afca71218f296c67d8e081d4c
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-13 15:36:57 -0600

    Update test_func.sql

--- functions/test_func.sql
+++ functions/test_func.sql
@@ -1,2 +1,14 @@
-SELECT order_id, getEmployeeName(employee_id) AS employee_name
-FROM orders;
+SELECT
+    employee_id,
+    getEmployeeName(employee_id)
+FROM
+    employees;
+
+
+SELECT
+    order_id,
+    getEmployeeName(employee_id),
+    order_date,
+    order_total
+FROM
+    orders;
