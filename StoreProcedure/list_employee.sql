8ee88036ce049b25d6d9cc13e3cf2c2c68720116
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-13 13:58:59 -0600

    Update list_employee.sql

--- StoreProcedure/list_employee.sql
+++ StoreProcedure/list_employee.sql
@@ -1,6 +1,23 @@
-CREATE OR REPLACE PROCEDURE getEmployeeList(p_cursor OUT SYS_REFCURSOR)
-IS
+CREATE OR REPLACE PROCEDURE get_employees_by_status
+(
+    p_status IN employees.status%TYPE,
+    RetVal     out sys_refcursor
+)
+AS
+   
 BEGIN
-  OPEN p_cursor FOR
-  SELECT * FROM employees;
+    -- Abrir el cursor
+    OPEN RetVal FOR
+    SELECT 
+    employee_id,
+    firstname,
+    lastname,
+    hire_date,
+    salary,
+    status
+    FROM employees
+    WHERE status = p_status;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
 END;
