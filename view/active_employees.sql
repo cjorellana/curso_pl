306bd8adce0a1ac322b522f938ce73b93f0dcbf1
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-13 15:20:05 -0600

    Update active_employees.sql

--- view/active_employees.sql
+++ view/active_employees.sql
@@ -1,4 +1,10 @@
-CREATE VIEW active_employees AS
-SELECT *
-FROM employees
-WHERE status = 'ACTIVE';
+CREATE OR REPLACE VIEW active_employees AS
+SELECT
+    employee_id as codigo,
+    firstname Nombres,
+    lastname Apellido,
+    hire_date as Fecha_inicio   
+FROM
+    employees 
+where
+    upper(status)=upper('active');
