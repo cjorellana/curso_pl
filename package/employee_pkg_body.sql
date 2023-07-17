763ee1e4463bc0b0e02745d1480cbc1826415793
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-17 16:04:48 -0600

    Update employee_pkg_body.sql

--- package/employee_pkg_body.sql
+++ package/employee_pkg_body.sql
@@ -1,22 +1,23 @@
-CREATE OR REPLACE PACKAGE BODY employee_pkg IS
-  -- Implementación de la función
-  FUNCTION getEmployeeName(p_employee_id IN NUMBER) 
-    RETURN VARCHAR2 
-  IS 
-    employee_name VARCHAR2(100);
-  BEGIN
-    SELECT firstname || ' ' || lastname 
-    INTO employee_name 
-    FROM employees 
-    WHERE employee_id = p_employee_id;
-  
-    RETURN employee_name;
-  EXCEPTION 
-    WHEN NO_DATA_FOUND THEN
-      RETURN NULL;
-  END getEmployeeName;
+create or replace PACKAGE BODY employee_pkg IS
+/* Funciones */
 
-  -- Implementaciones de los procedimientos
+    FUNCTION getEmployeeName(p_employee_id IN NUMBER) 
+      RETURN VARCHAR2 
+    IS 
+      employee_name VARCHAR2(100);
+    BEGIN
+      SELECT firstname || ' ' || lastname 
+      INTO employee_name 
+      FROM employees 
+      WHERE employee_id = p_employee_id;
+
+      RETURN employee_name;
+    EXCEPTION 
+      WHEN NO_DATA_FOUND THEN
+        RETURN NULL;
+    END getEmployeeName;
+
+  /* SP */
   PROCEDURE updateEmployeeSalary(p_employee_id IN NUMBER, p_new_salary IN NUMBER) 
   IS
   BEGIN
@@ -31,7 +32,8 @@ CREATE OR REPLACE PACKAGE BODY employee_pkg IS
     WHEN OTHERS THEN
       DBMS_OUTPUT.PUT_LINE('Ocurrió un error al intentar actualizar el salario del empleado.');
   END updateEmployeeSalary;
-
+  
+  /*listar empleados*/
   PROCEDURE getEmployeeList(p_cursor OUT SYS_REFCURSOR)
   IS
   BEGIN
@@ -40,12 +42,4 @@ CREATE OR REPLACE PACKAGE BODY employee_pkg IS
   END getEmployeeList;
 
 
-  PROCEDURE print_employee_details (employee_id_in NUMBER) IS
-    employee_name VARCHAR2(50);
-  BEGIN
-    employee_name := get_employee_name(employee_id_in);
-    DBMS_OUTPUT.PUT_LINE('Employee name: ' || employee_name);
-  END print_employee_details;
-  
 END employee_pkg;
-/
