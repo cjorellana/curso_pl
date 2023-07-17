d661ba71dd4ad52de8f032759532583a4db8ca41
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-17 14:48:05 -0600

    Update employee_pkg_body.sql

--- package/employee_pkg_body.sql
+++ package/employee_pkg_body.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE PACKAGE BODY employee_pkg IS
   -- Implementación de la función
   FUNCTION getEmployeeName(p_employee_id IN NUMBER) 
-  RETURN VARCHAR2 
+    RETURN VARCHAR2 
   IS 
     employee_name VARCHAR2(100);
   BEGIN
-    SELECT first_name || ' ' || last_name 
+    SELECT firstname || ' ' || lastname 
     INTO employee_name 
     FROM employees 
     WHERE employee_id = p_employee_id;
-
+  
     RETURN employee_name;
   EXCEPTION 
     WHEN NO_DATA_FOUND THEN
