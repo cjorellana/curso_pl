02c73c1435b7d8b089ccb0352737366886b4bf26
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-31 16:07:39 -0600

    Update employee_pkg_enc.sql

--- package/employee_pkg_enc.sql
+++ package/employee_pkg_enc.sql
@@ -5,6 +5,9 @@ create or replace PACKAGE employee_pkg IS
     -- Especificaciones para los procedimientos
     PROCEDURE updateEmployeeSalary(p_employee_id IN NUMBER, p_new_salary IN NUMBER);
     PROCEDURE getEmployeeList(p_cursor OUT SYS_REFCURSOR);
+    PROCEDURE getEmployeeSales(
+    p_employee_id IN employees.employee_id%type,
+    p_cursor OUT SYS_REFCURSOR); 
 END employee_pkg;
 
 
